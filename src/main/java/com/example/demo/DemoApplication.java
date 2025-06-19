6435587b814a72076c639b992ea9ddfe59be55e5
jsm5125 <jsm5125@naver.com>
2025-06-19 14:45:35 +0900

    commit test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
 
+		for(int i= 0 ; i<100; i++){
+			System.out.println("213sdfads");
+		}
+	}
 }
